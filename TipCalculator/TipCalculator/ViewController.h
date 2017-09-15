a176f0841b9fb395d8f2c4d0b5b9632b5a4f80e3
Paul <paul.huynh3@gmail.com>
2017-09-15 11:12:08 -0400

    begin

---
+++ TipCalculator/TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Paul on 2017-09-15.
+//  Copyright © 2017 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
